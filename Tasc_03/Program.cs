0e7b276c744238e0811b94caa3fbb40b11da700d
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-16 13:22:37 +0800

    add task_03

---
+++ Tasc_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+WriteLine("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(ReadLine());
+if (a == 1)
+{
+    WriteLine("Понедельник");
+}
+if (a == 2)
+{
+    WriteLine("Вторник");
+}
+if (a == 3)
+{
+    WriteLine("Среда");
+}
+if (a == 4)
+{
+    WriteLine("Четверг");
+}
+if (a == 5)
+{
+    WriteLine("Пятница");
+}
+if (a == 6)
+{
+    WriteLine("Суббота");
+}
+if (a == 7)
+{
+    WriteLine("Воскресенье");
+}
+if (a > 7)
+{
+    WriteLine("Недопустимое число");
+}
+if (a < 1)
+{
+    WriteLine("Недопустимое число");
+}
