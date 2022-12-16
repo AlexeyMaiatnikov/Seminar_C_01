118cbed0b8fc659e16c151e42376db4a3fb3b701
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-16 13:21:57 +0800

    add tasc_02 and task_03

--- Tasc_02/Program.cs
+++ Tasc_02/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(ReadLine());
+if (a == b * b)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
