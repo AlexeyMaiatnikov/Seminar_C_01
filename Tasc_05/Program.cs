1e299fc26df653764f41b4bebc2d345a35bdd47b
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-16 16:12:10 +0800

    add task_05

---
+++ Tasc_05/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+using static System.Console;
+WriteLine("Введите число трехзначное число: ");
+int number = Convert.ToInt32(ReadLine());
+int remainder = number % 10;
+Write($"{remainder}");
